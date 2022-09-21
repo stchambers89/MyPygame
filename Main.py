1cdb98bb11e216ed6fc759425b6b0c87571ed04a
stchambers89 <stchambers89@gmail.com>
2022-09-21 09:47:36 -0600

    Testing screen images

--- Main.py
+++ Main.py
@@ -7,12 +7,13 @@ pygame.init()
 # Basic Game Settings
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Etharis Island')
+font = pygame.font.Font('font/RetGanon.ttf', 50)
 
 clock =  pygame.time.Clock()
 
-testSurface = pygame.Surface((100,200))
-testSurface.fill('ForestGreen')
-
+skySurface= pygame.image.load('graphics/sky.png')
+groundSurface = pygame.image.load('graphics/ground.png')
+fontSurface = font.render('Test game', False, 'ForestGreen')
 
 # main game loop
 while True:
@@ -21,7 +22,9 @@ while True:
             pygame.quit()
             exit()
     
-    screen.blit(testSurface,(350,0))
-
+    screen.blit(skySurface, (200,0))
+    screen.blit(groundSurface, (0,300))
+    screen.blit(fontSurface, (300, 50))
+    
     pygame.display.update()
     clock.tick(60) #Capped game speed
