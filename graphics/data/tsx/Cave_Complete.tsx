c3a11bc5987ef9671a8c7a7afaaaaf774cb3c7ce
stchambers89 <stchambers89@gmail.com>
2022-09-27 14:22:42 -0600

    rplace test files

--- graphics/data/tsx/Cave_Complete.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="RGW_Cave_Complete" tilewidth="32" tileheight="32" tilecount="528" columns="24">
- <image source="../../tilesets/32x32/cave/RGW_Cave_Complete.png" width="768" height="704"/>
-</tileset>
---
+++ graphics/data/tsx/RGW_Cave_Complete_test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="RGW_Cave_Complete" tilewidth="16" tileheight="16" tilecount="2112" columns="48">
+ <image source="../../../../../../Desktop/School/Seinor Project/Sprites/Caves/RGW Caves/32x32/RGW_Cave_Complete.png" width="768" height="704"/>
+</tileset>
