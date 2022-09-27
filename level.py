72fd8fe378e477778e5602015c8ac54db871c9dd
stchambers89 <stchambers89@gmail.com>
2022-09-27 14:56:49 -0600

    Trying to do camera, so far no crash

--- level.py
+++ level.py
@@ -30,7 +30,7 @@ class Level:
             
         
     def run(self):
-        self.visable_sprites.custom_draw()
+        self.visable_sprites.custom_draw(self.player)
         self.visable_sprites.update()
 
 class YSortCameraGroup(pygame.sprite.Group):
@@ -38,9 +38,16 @@ class YSortCameraGroup(pygame.sprite.Group):
         super().__init__()
 
         self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2()
     
-    def custom_draw(self):
+    def custom_draw(self, player):
+
+        #getting offest
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+
         for sprite in self.sprites():
-            offset_pos = sprite.rect.topleft +self.offset
+            offset_pos = sprite.rect.topleft - self.offset
             self.display_surface.blit(sprite.image,offset_pos)
