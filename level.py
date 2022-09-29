edea249a09709d2758e0836e34f5b144f8342adf
stchambers89 <stchambers89@gmail.com>
2022-09-29 11:46:43 -0600

    boundry map, csv files from tiled program

--- level.py
+++ level.py
@@ -3,6 +3,7 @@ from settings import *
 from tile import Tile
 from player import Player
 from debug import debug
+from support import *
 
 class Level:
     
@@ -19,14 +20,23 @@ class Level:
         self.create_map()
     
     def create_map(self):
-        for row_index,row in enumerate(WORLD_MAP):
-            for col_index, col in enumerate(row):
-                x = col_index * TILESIZE
-                y = row_index * TILESIZE
-                if col == 'x':
-                    Tile((x,y), [self.visable_sprites, self.obstacle_sprites])
-                if col == 'p':
-                    self.player = Player((x,y), [self.visable_sprites], self.obstacle_sprites)
+        layouts = {
+            'boundary' : import_csv_layout('map\map_FloorBlocks.csv')
+        }
+        
+        for style,layout in layouts.items():
+            for row_index,row in enumerate(layout):
+                for col_index, col in enumerate(row):
+                    if col != '-1':
+                        x = col_index * TILESIZE
+                        y = row_index * TILESIZE
+                        if style == 'boundary':
+                            Tile((x,y), [self.visable_sprites, self.obstacle_sprites], 'invisable')
+        #         if col == 'x':
+        #             Tile((x,y), [self.visable_sprites, self.obstacle_sprites])
+        #         if col == 'p':
+        #             self.player = Player((x,y), [self.visable_sprites], self.obstacle_sprites)
+        self.player = Player((2000,1430), [self.visable_sprites], self.obstacle_sprites)
             
         
     def run(self):
